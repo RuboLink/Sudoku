bd127527fb837ad70043458868ad35f87ac9c52f
Rubén Martínez Sánchez <ruboms@gmail.com>
2026-03-03 11:45:36 +0100

    Start project

---
+++ scripts.js
@@ -0,0 +1,39 @@
+const mainGrid = document.getElementById("mainGrid")
+const createButton = document.getElementById("createGrid")
+createButton.style.display = "grid"
+const body = document.getElementsByTagName("body")
+
+let size = 9
+
+async function oneGrid(){
+    for(let x = 0; x < 9; x++){
+        for(let y = 0; y < 9; y++){
+            mainGrid.innerHTML += `
+                <input 
+                    class="typingBox" 
+                    type="number" 
+                    min="0" 
+                    max="9"
+                    data-row="${x}"
+                    data-col="${y}"
+                >`
+        }
+    }
+}
+
+async function corrections(){
+    let casillas = document.getElementsByClassName("typingBox");
+    casillas.forEach(n => {
+        console.log(n)
+    })
+}
+
+createButton.addEventListener("click", async () => {
+    mainGrid.style.display = "grid"
+    createButton.style.display = "none"
+    await oneGrid()
+    body[0].innerHTML += `<button id="checkResults">Corrígeme</button>`
+});
+
+const correct = getElementById("checkResults")
+
