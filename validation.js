bcf8d6f02459aa13087c13c0f47efd1adf687cf7
Rubén Martínez Sánchez <ruboms@gmail.com>
2026-03-09 20:40:07 +0100

    Create functions checkRow and checkColumn to the validation system

--- validation.js
+++ validation.js
@@ -1,34 +1,32 @@
 const casillas = document.getElementsByClassName("typingBox");
 
 async function getRows(){
-  
+  let filas = {}
+
   for(c of casillas){
     let x = c.dataset-row
-    let filas = {}
-
+    
     if(!filas[x]){
       filas[x] = []
     }
-    
+
     filas[x].push(c)
   }
-
   return filas
 }
 
 async function getColumns(){
+  let columnas = {}
 
   for(c of casillas){
     let y = c.dataset-col;
-    let columnas = {}
-
+    
     if(!columnas[y]){
       columnas[y] = []
     }
 
     columnas[y].push(c)
   }
-
   return columnas
 }
 
@@ -69,3 +67,37 @@ async function generarCoordenadas(){
 
 
 const zonas = await generarCoordenadas()
+
+async function checkRow(){
+  const filas = await getRows()
+  for(let i = 0; i < 9; i++){
+    let fila = filas[i];
+    const valores = fila.map(c => c.value);
+    const set = new Set(valores)
+
+    if(valores.includes("")){
+      return false
+    }
+    if(set.size !== valores.length){
+      return false;
+    }
+  }
+  return true
+}
+
+async function checkColumn(){
+  const columnas = await getColumns()
+  for(let i = 0; i < 9; i++){
+    let columna = columnas[i];
+    const valores = columna.map(c => c.value);
+    const set = new Set(valores)
+
+    if(valores.includes("")){
+      return false
+    }
+    if(set.size !== valores.length){
+      return false;
+    }
+  }
+  return true
+}
